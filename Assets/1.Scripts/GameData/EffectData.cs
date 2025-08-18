2fae50c6ea6a502e881b37bc5f459071fd684e32
이준호 <57254350+LKM0222@users.noreply.github.com>
2025-08-18 22:04:08 +0900

    Ch 02. 게임 시스템 제작 - 03. 이펙트 데이터 모델 1-2

--- Assets/1.Scripts/GameData/EffectData.cs
+++ Assets/1.Scripts/GameData/EffectData.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectData : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+// 추가한 라이브러리
+using System;
+using System.Xml;
+using System.IO;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+/// <summary>
+/// 이펙트 클립 리스트와 이펙트 파일 이름과 경로를 가지고 있음.
+/// 파일을 읽고, 쓰는 기능을 가짐.
+/// </summary>
+public class EffectData : BaseData
+{
+    // 배열은 정해둔 크기만 가지기 때문에, 여기서는 안전을 위해 배열을 사용.
+    public EffectClip[] effectClips = new EffectClip[0];
+    public string clipPath = "Effects/";
+    private string xmlFilePath = "";
+    private string xmlFileName = "effectData.xml";
+    private string dataPath = "Data/effectData";
+
+    // XML 구분자
+    private const string EFFECT = "effect"; // 저장 키
+    private const string CLIP = "clip"; // 저장 키
+
+    private EffectData() { }
 }
