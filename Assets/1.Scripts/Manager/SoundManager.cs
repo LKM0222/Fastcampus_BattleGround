ed51397f9af6b21d8ded5ff353a6305fed9c91a6
이준호 <57254350+LKM0222@users.noreply.github.com>
2025-08-19 23:33:13 +0900

    Ch 02. 게임 시스템 제작 - 11. 사운드 툴 마무리, 사운드 매니저1-2

--- Assets/1.Scripts/Manager/SoundManager.cs
+++ Assets/1.Scripts/Manager/SoundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -42,4 +43,135 @@ public class SoundManager : SingletonMonobehaviour<SoundManager>
     private SoundClip lastSound = null;
     private float minVolume = -80.0f;
     private float maxVolume = 0.0f;
+
+    private void Start()
+    {
+        if (mixer == null)
+        {
+            mixer = Resources.Load(MixerName) as AudioMixer;
+        }
+        if (audioRoot == null)
+        {
+            audioRoot = new GameObject(ContainerName).transform;
+            audioRoot.SetParent(transform);
+            audioRoot.localPosition = Vector3.zero;
+        }
+        if (fadeA_audio == null)
+        {
+            GameObject fadeA = new GameObject(FadeA, typeof(AudioSource));
+            fadeA.transform.SetParent(audioRoot);
+            fadeA_audio = fadeA.GetComponent<AudioSource>();
+            fadeA_audio.playOnAwake = false;
+        }
+        if (fadeB_audio == null)
+        {
+            GameObject fadeB = new GameObject(FadeB, typeof(AudioSource));
+            fadeB.transform.SetParent(audioRoot);
+            fadeB_audio = fadeB.GetComponent<AudioSource>();
+            fadeB_audio.playOnAwake = false;
+        }
+        if (UI_audio == null)
+        {
+            GameObject ui = new GameObject(UI, typeof(AudioSource));
+            ui.transform.SetParent(audioRoot);
+            UI_audio = ui.GetComponent<AudioSource>();
+            UI_audio.playOnAwake = false;
+        }
+        if (effect_audios == null || effect_audios.Length == 0)
+        {
+            effect_PlayStartTime = new float[EffectChannelCount];
+            effect_audios = new AudioSource[EffectChannelCount];
+            for (int i = 0; i < EffectChannelCount; i++)
+            {
+                effect_PlayStartTime[i] = 0.0f;
+                GameObject effect = new GameObject("Effect" + i.ToString(), typeof(AudioSource));
+                effect.transform.SetParent(audioRoot);
+                effect_audios[i] = effect.GetComponent<AudioSource>();
+                effect_audios[i].playOnAwake = false;
+            }
+        }
+
+        if (mixer != null)
+        {
+            fadeA_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(BGMGroupName)[0];
+            fadeB_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(BGMGroupName)[0];
+            UI_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(UIGroupName)[0];
+            for (int i = 0; i < effect_audios.Length; i++)
+            {
+                effect_audios[i].outputAudioMixerGroup = mixer.FindMatchingGroups(EffectGroupName)[0];
+            }
+        }
+
+        VolumeInit();
+    }
+
+    public void SetBGMVolume(float currentRatio)
+    {
+        currentRatio = Mathf.Clamp01(currentRatio);
+        float volume = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        mixer.SetFloat(BGMVolumeParam, volume);
+        PlayerPrefs.SetFloat(BGMVolumeParam, volume);
+    }
+
+    public float GetBGMVolume()
+    {
+        if (PlayerPrefs.HasKey(BGMVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(BGMVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
+    }
+
+    public void SetEffectVolume(float currentRatio)
+    {
+        currentRatio = Mathf.Clamp01(currentRatio);
+        float volume = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        mixer.SetFloat(EffectVolumeParam, volume);
+        PlayerPrefs.SetFloat(EffectVolumeParam, volume);
+    }
+
+    public float GetEffectVolume()
+    {
+        if (PlayerPrefs.HasKey(EffectVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(EffectVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
+    }
+
+    public void SetUIVolume(float currentRatio)
+    {
+        currentRatio = Mathf.Clamp01(currentRatio);
+        float volume = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        mixer.SetFloat(UIVolumeParam, volume);
+        PlayerPrefs.SetFloat(UIVolumeParam, volume);
+    }
+
+    public float GetUIVolume()
+    {
+        if (PlayerPrefs.HasKey(UIVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(UIVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
+    }
+
+    private void VolumeInit()
+    {
+        if (mixer != null)
+        {
+            mixer.SetFloat(BGMVolumeParam, GetBGMVolume());
+            mixer.SetFloat(EffectVolumeParam, GetEffectVolume());
+            mixer.SetFloat(UIVolumeParam, GetUIVolume());
+        }
+    }
 }
