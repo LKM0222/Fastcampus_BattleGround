519c6f38a86feb684070bb0cec14ec7cec01dc0a
이준호 <57254350+LKM0222@users.noreply.github.com>
2025-08-20 21:40:43 +0900

    Ch 04. 플레이어 제작 - 06. 조준동작 (aim behaviour) 마무리

--- Assets/1.Scripts/Player/AimBehaviour.cs
+++ Assets/1.Scripts/Player/AimBehaviour.cs
@@ -23,9 +23,12 @@ public class AimBehaviour : GenericBehaviour
     private Vector3 initialRootRotation; // 루트 본 로컬 회전값.
     private Vector3 initialHipRotation; // 
     private Vector3 initialSpineRotation; // 
+    private Transform myTransform;
 
     private void Start()
     {
+        myTransform = transform;
+
         // setup
         aimBool = Animator.StringToHash("Aim");
         cornerBool = Animator.StringToHash("Corner");
@@ -35,5 +38,125 @@ public class AimBehaviour : GenericBehaviour
         initialRootRotation = (hips.parent == transform) ? Vector3.zero : hips.parent.localEulerAngles;
         initialHipRotation = hips.localEulerAngles;
         initialSpineRotation = behaviourController.GetAnimator.GetBoneTransform(HumanBodyBones.Spine).localEulerAngles;
+    }
+
+    // 카메라에 따라 플레이어를 올바른 방향으로 회전
+    void Rotating()
+    {
+        Vector3 forward = behaviourController.playerCamera.TransformDirection(Vector3.forward);
+        forward.y = 0.0f;
+        forward = forward.normalized;
+
+        Quaternion targetRotation = Quaternion.Euler(0f, behaviourController.GetCamScript.GetH, 0.0f);
+        float minSpeed = Quaternion.Angle(myTransform.rotation, targetRotation) * aimTurnSmoothing;
+
+        if (peekCorner)
+        {
+            // 조준 중일때 플레이어 상체만 살짝 기울여주기 위함.
+            myTransform.rotation = Quaternion.LookRotation(-behaviourController.GetLastDirection());
+            targetRotation *= Quaternion.Euler(initialRootRotation);
+            targetRotation *= Quaternion.Euler(initialHipRotation);
+            targetRotation *= Quaternion.Euler(initialSpineRotation);
+            Transform spine = behaviourController.GetAnimator.GetBoneTransform(HumanBodyBones.Spine);
+            spine.rotation = targetRotation;
+        }
+        else
+        {
+            behaviourController.SetLastDirection(forward);
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, targetRotation, minSpeed * Time.deltaTime);
+        }
+    }
+
+    private void AimManagement()
+    {
+        Rotating();
+    }
+
+    // 조준 했다, 풀었다 하는 코루틴
+    private IEnumerator ToggleAimOn()
+    {
+        yield return new WaitForSeconds(0.05f);
+        //조준이 불가능한 상태일때에 대한 예외처리 (죽었거나, 특수한 상황(스턴)일경우)
+        if (behaviourController.GetTempLockStatus(behaviourCode) || behaviourController.IsOverriding(this))
+        {
+            yield return false;
+        }
+        else
+        {
+            aim = true;
+            int signal = 1;
+            if (peekCorner)
+            {
+                signal = (int)Mathf.Sign(behaviourController.GetH);
+            }
+            aimCamOffset.x = Mathf.Abs(aimCamOffset.x) * signal;
+            aimPivotOffset.x = Mathf.Abs(aimPivotOffset.x) * signal;
+
+            yield return new WaitForSeconds(0.1f);
+            behaviourController.GetAnimator.SetFloat(speedFloat, 0.0f);
+            behaviourController.OverrideWithBehaviour(this);
+        }
+    }
+
+    private IEnumerator ToggleAimOff()
+    {
+        aim = false;
+        yield return new WaitForSeconds(0.3f);
+        behaviourController.GetCamScript.ResetTargetOffsets();
+        behaviourController.GetCamScript.ResetMaxVertialAngle();
+        yield return new WaitForSeconds(0.1f);
+        behaviourController.RevokeOverridingBehaviour(this);
+    }
+
+    public override void LocalFixedUpdate()
+    {
+        if (aim)
+        {
+            behaviourController.GetCamScript.SetTargetOffset(aimPivotOffset, aimCamOffset);
+        }
+    }
+
+    public override void LocalLateUpdate()
+    {
+        AimManagement();
+    }
+
+    private void Update()
+    {
+        peekCorner = behaviourController.GetAnimator.GetBool(cornerBool);
+        // 버튼을 누르고 있을때만 조준한다.
+        if (Input.GetAxisRaw("Aim") != 0 && !aim)
+        {
+            StartCoroutine(ToggleAimOn());
+        }
+        else if (aim && Input.GetAxisRaw("Aim") == 0)
+        {
+            StartCoroutine(ToggleAimOff());
+        }
+
+        // 조준중엔 전력질주를 할 수 없음.
+        canSprint = !aim;
+
+        if (aim && Input.GetButtonDown("Aim Shoulder") && !peekCorner)
+        {
+            aimCamOffset.x = aimCamOffset.x * (-1);
+            aimPivotOffset.x = aimPivotOffset.x * (-1);
+        }
+        behaviourController.GetAnimator.SetBool(aimBool, aim);
+    }
+
+    private void OnGUI()
+    {
+        if (crossHair != null)
+        {
+            float length = behaviourController.GetCamScript.GetCurrentPivotMagnitude(aimPivotOffset);
+
+            if (length < 0.05f)
+            {
+                GUI.DrawTexture(new Rect(Screen.width * 0.5f - (crossHair.width * 0.5f),
+                                        Screen.height * 0.5f - (crossHair.height * 0.5f),
+                                        crossHair.width, crossHair.height), crossHair);
+            }
+        }
     }
 }
