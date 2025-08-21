05663e062c6d91ac9ad05efd33a36a8a7e8e89c5
이준호 <57254350+LKM0222@users.noreply.github.com>
2025-08-21 22:26:47 +0900

    Ch 03. Enemy Ai 시스템 만들기 - 03. EnemyVariables, EnemyAnimation 02

--- Assets/1.Scripts/Enemy/EnemyAnimation.cs
+++ Assets/1.Scripts/Enemy/EnemyAnimation.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FC;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -56,5 +57,143 @@ public class EnemyAnimation : MonoBehaviour
         }
     }
 
+    private void Setup(float speed, float angle, Vector3 strafeDirection)
+    {
+        angle *= Mathf.Deg2Rad;
+        angularSpeed = angle / controller.generalStats.angleResponseTime;
 
+        anim.SetFloat("Speed", speed, controller.generalStats.speedDampTime, Time.deltaTime);
+        anim.SetFloat("AngularSpeed", angularSpeed, controller.generalStats.angularSpeedDampTime, Time.deltaTime);
+
+        anim.SetFloat("H", strafeDirection.x, controller.generalStats.speedDampTime, Time.deltaTime);
+        anim.SetFloat("V", strafeDirection.z, controller.generalStats.speedDampTime, Time.deltaTime);
+    }
+
+    // navMeshAgen에 의해 anim이 셋업되는 함수.
+    private void NavAnimSetup()
+    {
+        float speed;
+        float angle;
+        speed = Vector3.Project(nav.desiredVelocity, transform.forward).magnitude;
+        if (controller.focusSight)
+        {
+            Vector3 dest = (controller.personalTarget - transform.position);
+            dest.y = 0.0f;
+            angle = Vector3.SignedAngle(transform.forward, dest, transform.up);
+            if (controller.Strafing)
+            {
+                dest = dest.normalized;
+                Quaternion targetStrafeRotation = Quaternion.LookRotation(dest);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetStrafeRotation, turnSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (nav.desiredVelocity == Vector3.zero)
+            {
+                angle = 0.0f;
+            }
+            else
+            {
+                angle = Vector3.SignedAngle(transform.forward, nav.desiredVelocity, transform.up);
+            }
+        }
+
+        // 플레이어를 향하려 할 때, 깜빡거리지 않도록 각도 데드존을 적용
+        if (!controller.Strafing && Mathf.Abs(angle) < controller.generalStats.angleDeadZone)
+        {
+            transform.LookAt(transform.position + nav.desiredVelocity);
+            angle = 0f;
+            if (pendingAim && controller.focusSight)
+            {
+                controller.Aiming = true;
+                pendingAim = false;
+            }
+        }
+
+        // Strafe Direction
+        Vector3 direction = nav.desiredVelocity;
+        direction.y = 0.0f;
+        direction = direction.normalized;
+        direction = Quaternion.Inverse(transform.rotation) * direction;
+        Setup(speed, angle, direction);
+    }
+
+
+    private void Update()
+    {
+        NavAnimSetup();
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (Time.timeScale > 0 && Time.deltaTime > 0)
+        {
+            nav.velocity = anim.deltaPosition / Time.deltaTime;
+            if (!controller.Strafing)
+            {
+                transform.rotation = anim.rootRotation;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (controller.Aiming)
+        {
+            Vector3 direction = controller.personalTarget - spine.position;
+            if (direction.magnitude < 0.01f || direction.magnitude > 1000000.0f)
+            {
+                return;
+            }
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            targetRotation *= Quaternion.Euler(initialRootRotation);
+            targetRotation *= Quaternion.Euler(initialHipsRotation);
+            targetRotation *= Quaternion.Euler(initialSpineRotation);
+
+            targetRotation *= Quaternion.Euler(VectorHelper.ToVector(controller.classStats.AimOffset));
+            Quaternion frameRotation = Quaternion.Slerp(lastRotation, targetRotation, timeCountAim);
+
+            // 엉덩이를 기준으로 척추 회전이 60도 이하인 경우는 계속 조준 가능.
+            if (Quaternion.Angle(frameRotation, hips.rotation) <= 60.0f)
+            {
+                spine.rotation = frameRotation;
+                timeCountAim += Time.deltaTime;
+            }
+            else
+            {
+                if (timeCountAim == 0 && Quaternion.Angle(frameRotation, hips.rotation) > 70.0f)
+                {
+                    StartCoroutine(controller.UnstuckAim(2f));
+                }
+
+                spine.rotation = lastRotation;
+                timeCountAim = 0;
+            }
+
+            lastRotation = spine.rotation;
+            Vector3 target = controller.personalTarget - gunMuzzle.position;
+            Vector3 forward = gunMuzzle.forward;
+            currentAnimingAngleGap = Vector3.Angle(target, forward);
+
+            timeCountGuard = 0;
+        }
+        else
+        {
+            lastRotation = spine.rotation;
+            spine.rotation *= Quaternion.Slerp(Quaternion.Euler(VectorHelper.ToVector(controller.classStats.AimOffset)), Quaternion.identity, timeCountGuard);
+            timeCountGuard += Time.deltaTime;
+        }
+    }
+
+    public void ActivatePendingAim()
+    {
+        pendingAim = true;
+    }
+    public void AbortPendingAim()
+    {
+        pendingAim = false;
+        controller.Aiming = false;
+    }
 }
