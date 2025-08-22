b17fe01265d35385b201894776aad0506910f688
LKM0222 <python03@naver.com>
2025-08-22 15:38:48 +0900

    Ch 04. Enemy Ai FSM 만들기 2 - 02. Action 05 (ReturnToCover, TakeCover)

---
+++ Assets/1.Scripts/Enemy/StateMachine/Action/ReturnToCoverAction.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/ReturnToCover")]
+public class ReturnToCoverAction : Action
+{
+    public override void OnReadyAction(StateController controller)
+    {   
+        // coverSpot이 존재한다면
+        if(!Equals(controller.CoverSpot, Vector3.positiveInfinity))
+        {
+            controller.nav.destination = controller.CoverSpot;
+            controller.nav.speed = controller.generalStats.chaseSpeed;
+            if(Vector3.Distance(controller.CoverSpot, controller.transform.position) > 0.5f)
+            {
+                controller.enemyAnimation.AbortPendingAim();
+            }
+        }
+        else
+        {
+            controller.nav.destination = controller.transform.position;
+        }
+    }
+
+    public override void Act(StateController controller)
+    {
+        if(!Equals(controller.CoverSpot, controller.transform.position))
+        {
+            controller.focusSight = false;
+        }
+    }
+}
